30df5a2442e31384c3b8dd0b7c495fbaa61d4888
yemliha81 <yemlihaddln@hotmail.com>
2025-08-31 18:33:33 +0300

    clear_cahe.php file added

---
+++ public/clear_cache.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+
+// Make sure we load the Laravel bootstrap
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+// Get the console kernel
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+// Run the Artisan commands
+Artisan::call('config:clear');
+echo Artisan::output();
+
+Artisan::call('cache:clear');
+echo Artisan::output();
+
+Artisan::call('route:clear');
+echo Artisan::output();
+
+Artisan::call('view:clear');
+echo Artisan::output();
+echo "Cache cleared successfully.\n";
