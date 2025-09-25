be291a67a1c835939b0175cf41167eba2a00d537
yemliha81 <yemlihaddln@hotmail.com>
2025-09-25 14:43:44 +0300

    Map url added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -100,7 +100,7 @@
         {
             latLng: { lat: <?= $item['lat'] ?>, lng: <?= $item['long'] ?> },
             zoom: 15,
-            marker: `<a href="https://maps.google.com" target="_blank" class="relative group flex items-center">
+            marker: `<a href="<?= $item['map_url'] ?>" target="_blank" class="relative group flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="h-[45px] w-auto animate-bounce"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" fill="#B6A36B"/></svg>
                     </a>`
         },
