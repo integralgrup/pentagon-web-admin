b9ef56949c53b3969eb43c5510768651f49e6798
yemliha81 <yemlihaddln@hotmail.com>
2025-10-09 16:36:01 +0300

    no index added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- add no index --> 
+    <meta name="robots" content="noindex,nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Açıklama alanı">
     <title><?php if (!empty($pageTitle)) echo $pageTitle . ' - '; ?><?= $nameofProject??'Pentagon Yapı'; ?></title>
