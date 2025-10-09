3a379f7175ddc5c6b2a7b3bc5c25c51aeb27c507
yemliha81 <yemlihaddln@hotmail.com>
2025-10-09 16:30:08 +0300

    ae direction rtl added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -23,6 +23,12 @@
         let FF_FOUC_FIX;
     </script>
 </head>
+@if(app()->getLocale() == 'ae')
+<style>
+    body{
+        direction: rtl;
+    }
+</style>
 <body class="antialiased font-sans [background:linear-gradient(0deg,_#FBFAF6_0%,_#FBFAF6_100%),_#FFF] [&.contact]:!bg-white group/body">
 <!-- Get Menu items from app\Models\Menu which is not deleted -->
 <?php $menuItems = App\Models\Menu::where(['lang' => app()->getLocale(), 'parent_menu_id' => 0, 'menu_type' => 'header'])->where('deleted_at', null)->orderBy('sort')->get(); ?>
