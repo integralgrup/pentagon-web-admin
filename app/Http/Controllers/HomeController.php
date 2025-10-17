3552543fe601d584bfe187bb520dbc991be8e9a8
yemliha81 <yemlihaddln@hotmail.com>
2025-10-17 11:02:16 +0300

    AE URL test 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -57,7 +57,7 @@ class HomeController extends Controller
         }
 
         $menu = Menu::where(['seo_url' => $slug, 'lang' => app()->getLocale()])->firstOrFail();
-        if($app()->getLocale() == 'ae') {
+        if(app()->getLocale() == 'ae') {
             dd($menu);
         }
         // If the menu item has a page_type of 'about', fetch the about data
