9c9182b4daee0b9a89df2caea00a95299da560a9
yemliha81 <yemlihaddln@hotmail.com>
2025-11-03 13:21:45 +0300

    RTL ae fix

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="tr">
+<html lang="tr" <?= (app()->getLocale() == 'ae') ? 'dir="rtl"' : 'dir="ltr"'?>   >
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
